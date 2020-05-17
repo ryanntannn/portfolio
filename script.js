cea975f25fd4585db7718bd63f0082eb1a2e5d59
ryanntannn <tanryan777@gmail.com>
2020-05-17 17:10:45 +0800

    Init

---
+++ script.js
@@ -0,0 +1,10 @@
+var currentVisible = null
+
+function MakeVisible(elementId){
+    if(currentVisible){
+        currentVisible.style.display = "none";
+    }
+
+    currentVisible = document.getElementById(elementId);
+    currentVisible.style.display = "block";
+}
